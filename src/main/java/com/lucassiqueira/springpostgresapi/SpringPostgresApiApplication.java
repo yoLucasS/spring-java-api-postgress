9fb1f3957d6e62d7aadf9a64099016d981387471
Lucas Siqueira <lucas.siqueira@rpinfo.com.br>
2023-12-01 15:56:41 -0300

    First Commit

---
+++ src/main/java/com/lucassiqueira/springpostgresapi/SpringPostgresApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucassiqueira.springpostgresapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgresApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucassiqueira/springpostgresapi/SpringPostgresApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucassiqueira.springpostgresapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
